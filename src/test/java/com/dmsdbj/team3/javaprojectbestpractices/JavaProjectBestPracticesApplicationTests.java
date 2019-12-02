f0e12c13cf8df7c36baabfc6d6ffd0be5bf3ed09
Sunshinezmh <18333640045@163.com>
2019-12-02 15:24:22 +0800

    添加pom依赖-sunshine-2019年12月02日15:20:47

--- src/test/java/com/dmsdbj/team3/javaprojectbestpractices/JavaProjectBestPracticesApplicationTests.java
+++ src/test/java/com/dmsdbj/team3/javaprojectbestpractices/JavaProjectBestPracticesApplicationTests.java
@@ -1,6 +1,6 @@
 package com.dmsdbj.team3.javaprojectbestpractices;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
