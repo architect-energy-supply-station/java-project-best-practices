39e931d030cfdb7c07018dc8f4e3fe324896b990
cookr <shuai_0303@163.com>
2019-12-05 17:46:14 +0800

    添加MDC

--- src/main/java/com/dmsdbj/team3/javaprojectbestpractices/annotation/WebLogAspect.java
+++ src/main/java/com/dmsdbj/team3/javaprojectbestpractices/annotation/WebLogAspect.java
@@ -1,11 +1,13 @@
 package com.dmsdbj.team3.javaprojectbestpractices.annotation;
 
+import com.baomidou.mybatisplus.core.toolkit.IdWorker;
 import com.google.gson.Gson;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -28,6 +30,8 @@ public class WebLogAspect {
 
     protected final Logger logger = LoggerFactory.getLogger(this.getClass());
     private final static String LINE_SEPARATOR = System.lineSeparator();
+    private static final String TRACE_ID = "TRACE_ID";
+
 
     @Pointcut("@annotation(com.dmsdbj.team3.javaprojectbestpractices.annotation.WebLog)")
     public void WebLog() {
@@ -35,12 +39,14 @@ public class WebLogAspect {
 
     @Around("WebLog()")
     public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-        logger.info("_+_+_+_+_+_+_+_+_+_+_)+)+)+_+_      Around    _+_+_+_+_+_+++__+_+_+_+_");
+        MDC.put(TRACE_ID, IdWorker.getIdStr());
+        logger.info("Begin Around   ");
         long currentTimeMillis = System.currentTimeMillis();
         Object result = proceedingJoinPoint.proceed();
         //打印出参
-        logger.info("-----------------response Args : {}", new Gson().toJson(result));
-        logger.info("+++++++++++++++++Time consuming : {} ms", System.currentTimeMillis() - currentTimeMillis);
+        logger.info("response Args : {}", new Gson().toJson(result));
+        logger.info("Time consuming : {} ms", System.currentTimeMillis() - currentTimeMillis);
+        MDC.remove(TRACE_ID);
         return result;
     }
 
@@ -54,7 +60,7 @@ public class WebLogAspect {
         //获取WebLog注解的描述信息
         String methodDescription = getAspectLogDescription(joinpoint);
         //打印相关参数
-        logger.info("++++++++++++++       start        ++++++++++++++");
+        logger.info("    start        ");
         logger.info("URL            :{}", request.getRequestURL());
         logger.info("Description    :{}", methodDescription.toString());
         logger.info("HTTP Method     :{}", request.getMethod());
@@ -67,7 +73,7 @@ public class WebLogAspect {
     @After("WebLog()")
     public void doAfter(JoinPoint joinpoint) {
         //接口结束后换行，方便分割查看
-        logger.info("==========================End===============" + LINE_SEPARATOR);
+        logger.info("It's The End" + LINE_SEPARATOR);
     }
 
 
