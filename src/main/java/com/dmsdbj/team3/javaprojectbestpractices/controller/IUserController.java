756b03a6da96e3ec71997a99e29b8022194fd0de
13_lina <15831687415@163.com>
2019-12-07 10:04:42 +0800

    提交BeanValidation-2019年12月6日-李娜

--- src/main/java/com/dmsdbj/team3/javaprojectbestpractices/controller/IUserController.java
+++ src/main/java/com/dmsdbj/team3/javaprojectbestpractices/controller/IUserController.java
@@ -1,5 +1,4 @@
 package com.dmsdbj.team3.javaprojectbestpractices.controller;
-import	java.lang.annotation.Annotation;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
@@ -8,8 +7,11 @@ import com.dmsdbj.team3.javaprojectbestpractices.utils.ResultBean;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 /**
@@ -18,28 +20,29 @@ import java.util.List;
  * @since 0.0.1  2019-12-02
  */
 @Api(tags = {"用户表接口"})
+@Validated
 public interface IUserController {
     @ApiOperation(value = "根据Id查询用户", notes = "请输入用户ID")
     @GetMapping("/user/info/{id}")
-    ResultBean<User> getUser(@ApiParam(value = "id", required = true,defaultValue = "1") @PathVariable("id") int id);
+    ResultBean<User> getUser(@ApiParam(value = "id", required = true,defaultValue = "1") @NotNull @PathVariable("id") int id);
 
     @ApiOperation(value = "查询所有用户")
     @GetMapping("/user/list")
-    ResultBean<IPage> getUserList(@ApiParam(value = "page") Page page);
+    ResultBean<IPage> getUserList(@ApiParam(value = "page") @NotNull @Valid Page page);
 
     @ApiOperation(value = "根据UserId删除一个用户", notes = "请输入主键id进行查询")
     @DeleteMapping("/user/remove/{id}")
-    ResultBean<String> removeUser(@ApiParam(value = "id", required = true,defaultValue = "2") @PathVariable("id") int id);
+    ResultBean<String> removeUser(@ApiParam(value = "id", required = true,defaultValue = "2") @NotNull @PathVariable("id") int id);
 
     @ApiOperation(value = "新增一个用户", notes = "请输入要新增的用户信息")
     @PostMapping("/user/save")
-    ResultBean<String> saveUser(@ApiParam(value = "user") @RequestBody User user);
+    ResultBean<String> saveUser(@ApiParam(value = "user") @Valid @RequestBody User user);
 
     @ApiOperation(value = "根据姓名模糊查询", notes = "请输入要模糊查询的信息")
     @GetMapping(value = {"/user/getUserByLikeName/{name}"})
-    ResultBean<List<User>> getUserByLikeName(@ApiParam(value = "name", required = true,defaultValue ="xiaoli") @PathVariable String name);
+    ResultBean<List<User>> getUserByLikeName(@ApiParam(value = "name",required = true,defaultValue ="xiaoli") @NotBlank @NotNull @PathVariable String name);
 
     @ApiOperation(value = "更新用户手机号")
     @PostMapping("/user/updateUserByPhone/{oldPhone}/{newPhone}")
-    ResultBean updateUserByPhone(@PathVariable String oldPhone, @PathVariable String newPhone);
+    ResultBean updateUserByPhone(@NotBlank @PathVariable String oldPhone, @NotBlank @PathVariable String newPhone);
 }
--- src/main/java/com/dmsdbj/team3/javaprojectbestpractices/entity/User.java
+++ src/main/java/com/dmsdbj/team3/javaprojectbestpractices/entity/User.java
@@ -3,6 +3,14 @@ package com.dmsdbj.team3.javaprojectbestpractices.entity;
 import com.baomidou.mybatisplus.annotation.*;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 /**
  * <p>
@@ -14,6 +22,8 @@ import lombok.Data;
  */
 @TableName("user")
 @Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
 public class User {
 
     /**
@@ -22,11 +32,21 @@ public class User {
     @TableId(type = IdType.AUTO)
     @ApiModelProperty(value="主键id",example = "1")
     private long id;
+
+    @NotBlank(message = "用户名不能为空")
+    @Length(min=6,max=20,message = "用户名需要在6-20个字符")
+    @Pattern(regexp = "^[\\u4E00-\\u9FA5A-Za-z0-9\\*]*$", message = "用户昵称限制：最多20字符，包含文字、字母和数字")
     private String name;
 
-    @ApiModelProperty(value="年龄",example = "22")
+    @NotNull
     private int age;
+
+    @NotBlank(message = "邮箱不能为空")
+    @Email(message = "邮箱格式不正确")
     private String email;
+
+    @NotBlank(message = "手机号码不能为空")
+    @Pattern(regexp = "^[1][3,4,5,6,7,8,9][0-9]{9}$", message = "手机号格式有误")
     private String phone;
     /**
      * 此处故意用了类字段名和数据库列明不相符
@@ -34,4 +54,7 @@ public class User {
      */
     @TableField("userEvaluation")
     private String evaluation;
+
+    @TableLogic
+    private Integer deleted = 0;
 }
