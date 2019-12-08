c666d33f5a53fd6a4a55e6071eb4307c5c85bbdd
Sunshinezmh <18333640045@163.com>
2019-12-08 19:21:22 +0800

    监控-sunshine-2019年12月08日19:21:17

--- src/main/java/com/dmsdbj/team3/javaprojectbestpractices/JavaProjectBestPracticesApplication.java
+++ src/main/java/com/dmsdbj/team3/javaprojectbestpractices/JavaProjectBestPracticesApplication.java
@@ -1,8 +1,12 @@
 package com.dmsdbj.team3.javaprojectbestpractices;
 
 import com.github.sunshinezmh.swaggerspringbootautoconfigure.EnableSunshineSwagger2;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableSunshineSwagger2
@@ -10,6 +14,11 @@ public class JavaProjectBestPracticesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaProjectBestPracticesApplication.class, args);
+	}
+
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName){
+		return registry -> registry.config().commonTags("application", applicationName);
 	}
 
 }
