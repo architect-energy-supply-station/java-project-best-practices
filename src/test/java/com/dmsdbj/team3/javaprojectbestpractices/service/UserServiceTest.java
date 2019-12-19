a7de04b696691f083445cfda291f0ec3844811a0
cookr <shuai_0303@163.com>
2019-12-19 09:20:35 +0800

    using prometheus

---
+++ src/test/java/com/dmsdbj/team3/javaprojectbestpractices/service/UserServiceTest.java
@@ -0,0 +1,13 @@
+package com.dmsdbj.team3.javaprojectbestpractices.service;
+
+/**
+ * @ClassName java-project-best-practices
+ * @Author cookr
+ * @Date 2019/12/16 1:46 上午
+ * @Version 1.0
+ * @Description
+ **/
+
+
+public class UserServiceTest {
+}
