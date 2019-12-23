f371ce9a67ca56fca99159a43a3488b7ae178470
cookr <shuai_0303@163.com>
2019-12-23 17:36:57 +0800

    using flyway

---
+++ src/main/resources/db/migration/V1.0.1__instertUser.sql
@@ -0,0 +1,1 @@
+INSERT INTO `user` VALUES (14, '141414string', 14, '1414string', '1414string', '1414string', 0, '2019-11-29 01:06:19', '2019-11-29 01:06:19', '{\"city\":\"string\",\"sex\":\"string\"}');
